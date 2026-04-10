2bea340dbdce516a84b5030440ce56952b9b041d
Bryan Williams <bwillwork@proton.me>
2026-04-10 17:06:06 -0400

    Converting the code to be more functional

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,80 @@
 // Import our custom CSS
 import '../scss/styles.scss';
-import DOM from './dom';
 import _ from 'lodash';
 import generator from './qrcodeGenerator';
 
 // Import only the Bootstrap components we need
-import { Popover } from 'bootstrap';
-import tab from "bootstrap/js/src/tab";
+import {Popover} from 'bootstrap';
 import {disableQRCodeControls, enableQRCodeControls} from "./ui";
 import {filled, valid} from "./input";
 import {buildElementCache} from "./cache";
-import {tabIdMap, selectors, selectorFromId} from "./constants";
+import {selectors, tabIdMap} from "./constants";
 
 (function init() {
+
+
+    const elmCache = buildElementCache(selectors);
+
+
+
+    function fork(conditionFunc, ifFunc, elseFunc) {
+        return function(...values) {
+            if(conditionFunc(...values)) return ifFunc(...values);
+            else elseFunc(...values);
+        };
+    }
+
+    function choose(conditionFunc,...choices) {
+        return function(condition) {
+            for(let choice of choices) {
+                const {key, execFunc} = choice;
+                if(conditionFunc(condition,key)) return execFunc();
+            }
+            return null;
+        };
+    }
 
     function isTabActive(elmCache,id) {
         const tabs = elmCache.getElementFromSelector(selectors.allTabs);
         const activeTab = tabs.find(t => t.classList.contains('active'));
         return _.isEqual(activeTab.id, id);
     }
+
+    function isActiveAndValid(elmCache,id,isTabActiveFunc,isValidFunc) {
+        return function(currentTabId,...dataElementIds) {
+            const isActive = isTabActiveFunc(elmCache,currentTabId);
+            const isValid = isValidFunc(elmCache,...dataElementIds);
+            return isActive && isValid;
+        };
+    }
+
+    function linkIsValid(elmCache,linkSelector) {
+        const linkInput = elmCache.getElementFromSelector(linkSelector)[0];
+        return filled(linkInput) && valid(linkInput)
+    }
+    const linkIsValidAndActiveFunc = isActiveAndValid(
+        elmCache,
+        tabIdMap.link,
+        isTabActive,
+        linkIsValid);
+
+    const linkGeneratorFunc = fork(
+        (elmCache,linkSelector,tabId) => linkIsValidAndActiveFunc(tabId,linkSelector),
+        function(elmCache,linkSelector) {
+            const linkInput = elmCache.getElementFromSelector(linkSelector)[0];
+            const downloadBtn = elmCache.getElementFromSelector(selectors.downloadBtn)[0];
+            const noDataMessage = elmCache.getElementFromSelector(selectors.noDataMessage)[0];
+            const canvas = elmCache.getElementFromSelector(selectors.canvas)[0];
+
+            enableQRCodeControls({downloadBtn,noDataMessage,canvas});
+            generator.generate(canvas, linkInput.value);
+        },
+        function(elmCache) {
+            const downloadBtn = elmCache.getElementFromSelector(selectors.downloadBtn)[0];
+            const noDataMessage = elmCache.getElementFromSelector(selectors.noDataMessage)[0];
+            const canvas = elmCache.getElementFromSelector(selectors.canvas)[0];
+            disableQRCodeControls({downloadBtn,noDataMessage,canvas});
+        });
 
     function generateLinkQRCode(elmCache) {
         const isActive = isTabActive(elmCache,tabIdMap.link);
@@ -36,7 +92,7 @@ import {tabIdMap, selectors, selectorFromId} from "./constants";
     }
 
 
-    const elmCache = buildElementCache(selectors);
+
 
     // Create popovers (bootstrap)
     const popovers = elmCache.getElementFromSelector(selectors.popovers);
@@ -50,13 +106,13 @@ import {tabIdMap, selectors, selectorFromId} from "./constants";
             const activeId = event.target.getAttribute('id');
             const previousId = event.relatedTarget.getAttribute('id');
 
-        })
+        });
     });
 
 
     const linkInput = elmCache.getElementFromSelector(selectors.link)[0];//DOM.elm(`#link-input`);
-    generateLinkQRCode(elmCache);
-    linkInput.addEventListener('keyup', () => generateLinkQRCode(elmCache));
+    linkGeneratorFunc(elmCache, selectors.link, tabIdMap.link);
+    linkInput.addEventListener('keyup', () => linkGeneratorFunc(elmCache, selectors.link, tabIdMap.link));
 
 
 })();
